f0d1a1e45bd1ce5e6653a1fbf5bc968eee70cee1
Simisticot <simi.pra@gmail.com>
2025-03-06 23:25:09 +0100

    Respond to uci command

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
 use std::io;
 
 fn main() -> io::Result<()> {
+    let mut out = io::stdout();
     loop {
         let mut buffer = String::new();
         let _ = io::stdin().read_line(&mut buffer)?;
-        let command = to_uci_command(buffer.trim_end()).unwrap();
-        println!("{command:?}");
+        let command = to_uci_command_in(buffer.trim_end()).unwrap();
+        execute_command(command, &mut out);
     }
 }
 
 #[derive(PartialEq, Debug)]
-enum UciCommand {
+enum UciCommandIn {
     Uci,
 }
 
-fn to_uci_command(input: &str) -> Result<UciCommand, String> {
+fn to_uci_command_in(input: &str) -> Result<UciCommandIn, String> {
     match input {
-        "uci" => Ok(UciCommand::Uci),
+        "uci" => Ok(UciCommandIn::Uci),
         _ => Err("Unsupported command".to_string()),
+    }
+}
+
+fn execute_command(command: UciCommandIn, out: &mut impl io::Write) {
+    match command {
+        UciCommandIn::Uci => {
+            out.write_all("id name chessticot\n".as_bytes()).unwrap();
+            out.write_all("id author Simisticot\n".as_bytes()).unwrap();
+        }
     }
 }
 
@@ -27,11 +37,20 @@ mod tests {
 
     #[test]
     fn non_existent_uci_command_returns_error() {
-        assert!(to_uci_command("skibidi").is_err());
+        assert!(to_uci_command_in("skibidi").is_err());
     }
 
     #[test]
     fn can_parse_uci_command() {
-        assert!(to_uci_command("uci").is_ok_and(|command| command == UciCommand::Uci))
+        assert!(to_uci_command_in("uci").is_ok_and(|command| command == UciCommandIn::Uci));
+    }
+
+    #[test]
+    fn responds_to_uci_command() {
+        let mut out: Vec<u8> = Vec::new();
+
+        execute_command(UciCommandIn::Uci, &mut out);
+
+        assert_eq!(&out, b"id name chessticot\nid author Simisticot\n");
     }
 }
